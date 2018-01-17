dd684fe079cd0ad2ea3611eeb1effcd407cf3db5
Joshua Cantero <cantero.joshua@gmail.com>
2018-01-16 22:58:59 -0800

    Last commit

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -15,7 +15,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CalculatorPage {
 
+  height: number;
+  weight: number;
+  bmiValue: number;
+  bmiMessage: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  calculateBMI(){
+    if(this.weight > 0 && this.height > 0){
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
+
+  private setBMIMessage(){
+    if(this.bmiValue < 18.5){
+      this.bmiMessage = "Underweight";
+    }else if(this.bmiValue > 18.5 && this.bmiValue < 25){
+      this.bmiMessage = "Normal";
+    }else if(this.bmiValue > 25 && this.bmiValue < 30){
+      this.bmiMessage = "Overweight";
+    }else if(this.bmiValue > 30){
+      this.bmiMessage = "Obese";
+    }
   }
 
   ionViewDidLoad() {
