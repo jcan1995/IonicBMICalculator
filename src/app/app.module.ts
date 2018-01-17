5385389b6b89e5e1bb6bcaf45ac3c5147877a704
Joshua Cantero <cantero.joshua@gmail.com>
2018-01-16 22:21:29 -0800

    removed some scaffolded pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -15,8 +14,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage
   ],
   imports: [
@@ -27,8 +24,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage
   ],
   providers: [
